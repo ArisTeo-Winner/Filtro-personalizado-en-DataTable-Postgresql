cb234a8d80f240fe76e129ec27d6b4d752018ecf
Aristeo Ortiz Martinez <57931633+ArisTeo-Winner@users.noreply.github.com>
2021-10-03 06:25:26 -0500

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 
 $hostname = ""; //Ruta del servidor 
 $dbname = ""; // Nombre de base de daos
-$username = ""; // Usuario del servidor 
-$pass = "";// Contraseña del servidor 
+$username = ""; // Usuario 
+$pass = "";// Contraseña
 
 // Create connection
 $con = pg_connect(" host = $hostname dbname = $dbname user = $username password = $pass ");
